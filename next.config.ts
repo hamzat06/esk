f209b54ca010648309a0d35eae77362540dae7d4
Hamzat Ajanaku <hamzatajanaku@gmail.com>
2026-06-20 19:43:58 +0100

    feat: fix cloudinary images whitelisting

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
